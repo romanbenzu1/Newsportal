05a4b751fb890ca1840159ad8fac81c0ebdd62b2
Roman <romanbenzu@gmail.com>
2026-02-10 10:29:46 -0800

    article CRUD added

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,12 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 
-class Category extends Model
+class Article extends Model
 {
-    public function articles(): BelongsToMany
+   public function categories(): BelongsToMany
     {
-        return $this->belongsToMany(Article::class);
+        return $this->belongsToMany(Category::class);
     }
 }
-
-
